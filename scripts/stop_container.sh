7c7e258729a902cbf4778397a7ce89faf62abb9f
negi-1998 <69214409+negi-1998@users.noreply.github.com>
2024-07-27 17:43:06 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,14 +1,8 @@
-
-# set -e
-# containerid=` docker ps | awk -F " "'{print $1}'`
-# sudo docker rm -f $containerid
 #!/bin/bash
 set -e
 
-#Get the IDs of all running containers
 container_ids=$(docker ps -q)
 
-# Remove all running containers
 if [ -n "$container_ids" ]; then
   sudo docker rm -f $container_ids
 else
